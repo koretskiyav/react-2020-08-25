d02abfb8388856e0f42a825e2942adbe67d962d2
“Eugeniy <eugeniy@fivetran.com>
2020-09-05 20:59:28 +0300

    hometask(ht2): Add test for decrement product amount with increment

--- src/components/product/product.test.js
+++ src/components/product/product.test.js
@@ -22,6 +22,15 @@ describe('Product', () => {
     wrapper.find('[data-id="product-increment"]').simulate('click');
     expect(wrapper.find('[data-id="product-amount"]').text()).toBe('1');
   });
+  it('should decrement amount', () => {
+    const wrapper = mount(<Product product={product} />);
+    wrapper
+      .find('[data-id="product-increment"]')
+      .simulate('click')
+      .simulate('click');
+    wrapper.find('[data-id="product-decrement"]').simulate('click');
+    expect(wrapper.find('[data-id="product-amount"]').text()).toBe('1');
+  });
   it('should increment amount', () => {
     const fn = jest.fn();
     mount(<Product product={product} fetchData={fn} />);
