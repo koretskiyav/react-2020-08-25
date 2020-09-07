ddb2f23a19e3429691b85579e4b4fc630a7b5593
asosunoff <al.sosunoff@gmail.com>
2020-09-07 20:09:30 +0300

    useMemo

--- src/components/restaurant.js
+++ src/components/restaurant.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Menu from './menu';
 import Reviews from './reviews';
 import Rate from './rate';
 
 export default function Restaurant({ activeRestaurant }) {
-  const averageRating = Math.ceil(
-    activeRestaurant.reviews
-      .map(({ rating }) => rating)
-      .reduce((a, c) => a + c) / activeRestaurant.reviews.length
+  const averageRating = useMemo(
+    () =>
+      Math.ceil(
+        activeRestaurant.reviews
+          .map(({ rating }) => rating)
+          .reduce((a, c) => a + c) / activeRestaurant.reviews.length
+      ),
+    [activeRestaurant.reviews]
   );
 
   return (
