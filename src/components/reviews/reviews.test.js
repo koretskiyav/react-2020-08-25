91363997a6b6f397ac11ff19402802dafb6cfacd
asosunoff <al.sosunoff@gmail.com>
2020-09-07 22:06:25 +0300

    added test to reviews

--- src/components/reviews/reviews.test.js
+++ src/components/reviews/reviews.test.js
@@ -1,30 +1,22 @@
-/* import React from 'react'; */
-/* import Enzyme , { mount } from 'enzyme'; */
-/* import Adapter from 'enzyme-adapter-react-16'; */
-/* import Product from './product';
+import React from 'react';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Reviews from './reviews';
 import { restaurants } from '../../fixtures';
 
-const product = restaurants[0].menu[0]; */
+const reviews = restaurants[0].reviews;
 
-/* Enzyme.configure({ adapter: new Adapter() }); */
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('Reviews', () => {
-  /* it('should render', () => {
-    const wrapper = mount(<Product product={product} />);
-    expect(wrapper.find('[data-id="product"]').length).toBe(1);
+  it('should render', () => {
+    const wrapper = mount(<Reviews reviews={reviews} />);
+    expect(wrapper.find('[data-reviews="reviews"]').length).toBe(1);
   });
-  it('should init with 0 amount', () => {
-    const wrapper = mount(<Product product={product} />);
-    expect(wrapper.find('[data-id="product-amount"]').text()).toBe('0');
+  it('should class reviews', () => {
+    const wrapper = mount(<Reviews reviews={reviews} />);
+    expect(wrapper.find('[data-reviews="reviews"]').hasClass('reviews')).toBe(
+      true
+    );
   });
-  it('should increment amount', () => {
-    const wrapper = mount(<Product product={product} />);
-    wrapper.find('[data-id="product-increment"]').simulate('click');
-    expect(wrapper.find('[data-id="product-amount"]').text()).toBe('1');
-  });
-  it('should increment amount', () => {
-    const fn = jest.fn();
-    mount(<Product product={product} fetchData={fn} />);
-    expect(fn).toBeCalledWith(product.id);
-  }); */
 });
