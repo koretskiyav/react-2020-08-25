7d554c244f2d268897646b2a452f5d647c5dbbf7
asosunoff <al.sosunoff@gmail.com>
2020-09-09 20:08:50 +0300

    added redux devtools extansion

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,18 @@
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import reducer from './reducer';
 
-const store = createStore(reducer);
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(reduxThunk))
+);
 
 export default store;
