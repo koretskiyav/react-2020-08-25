230b904cc47a536e8a08bdf60dfeadd75cec63d1
asosunoff <al.sosunoff@gmail.com>
2020-09-13 14:35:05 +0300

    added review

--- src/redux/middlewares/generateId.js
+++ src/redux/middlewares/generateId.js
@@ -1,19 +1,5 @@
 import { v4 as uuidv4 } from 'uuid';
 
-/* export default (store) => (next) => (action) => {
-  const { type } = action;
-  switch (type) {
-    case ADD_REVIEW: {
-      action.payload.user.id = uuidv4();
-      action.payload.review.id = uuidv4();
-      next(action);
-      break;
-    }
-    default:
-      next(action);
-  }
-}; */
-
 export default function (nameField, ...forType) {
   return (store) => (next) => (action) => {
     if (forType && forType.includes(action.type)) {
