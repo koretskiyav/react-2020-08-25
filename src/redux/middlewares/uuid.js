c523119300321c7f4c064801f75f17aa117d5444
“Eugeniy <eugeniy@fivetran.com>
2020-09-14 12:22:38 +0300

    ht4(hometask): Generate uuids

--- src/redux/middlewares/uuid.js
+++ src/redux/middlewares/uuid.js
@@ -1,8 +1,14 @@
 import { SUBMIT } from '../constants';
+import { v4 as uuidv4 } from 'uuid';
 
 export default (store) => (next) => (action) => {
   if (action.type === SUBMIT) {
-    action.payload = { ...action.payload, id: 'new' };
+    action.payload = {
+      text: action.payload.text,
+      userId: uuidv4(),
+      id: uuidv4(),
+      rating: action.payload.rate,
+    };
   }
   next(action);
 };
